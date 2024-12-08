269342a70edf59a840ab63cbbed77b3dfe244e8d
Anton Karpukhin <a.karpukhin21@mail.ru>
2024-12-08 22:43:48 +0300

    Part 1

---
+++ 4-order-api/migrations/auto.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"orderApi/iternal/product"
+	"os"
+)
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		panic("Error loading .env file")
+	}
+	db, err := gorm.Open(postgres.Open(os.Getenv("DSN")), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	db.AutoMigrate(&product.Product{})
+}
